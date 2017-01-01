0fc1ef71813eadff4c026c23474b2614e771ecfa
Pranav Vashi <neobuddy89@gmail.com>
2017-01-01 12:52:12 +0530

    crdroid v1.9
    Change-Id: Ic3d5e3950ff05d0e9ca65ca0113bfb7fe90e0b63
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- config/common.mk
+++ config/common.mk
@@ -279,7 +279,7 @@ PRODUCT_VERSION_MAJOR = 7
 PRODUCT_VERSION_MINOR = 1.1
 
 # Increase CR Version with each major release.
-CR_VERSION := 1.8
+CR_VERSION := 1.9
 
 
 CM_VERSION := crDroidAndroid-$(PRODUCT_VERSION_MAJOR).$(PRODUCT_VERSION_MINOR)-$(shell date -u +%Y%m%d)-$(CM_BUILD)-v$(CR_VERSION)
